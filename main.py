8a87a2062d62438e85b8606674b30450ab4be751
Conrad H. Carl <coheca0815@gmail.com>
2024-07-31 19:28:07 +0200

    switch to opencv for rendering and saving the image

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     size = 1024
     inital_state = np.zeros(shape=(size, size), dtype=np.int8)
     apply_pattern(512, 512, gosper_glider_gun_pattern(), inital_state)
-    automaton(iterations=1000, s=size, interactive=False, show=True, save=False, initial_state=inital_state)
+    automaton(iterations=1000, s=size, interactive=False, show=True, save=True, initial_state=inital_state)
 
 if __name__ == '__main__':
     main()
--- src/renderer.py
+++ src/renderer.py
@@ -1,23 +1,25 @@
 import matplotlib.pyplot as plt
 import cv2
 import io
+import numpy as np
 
 def render_frame(grid, filename):
-    plt.figure(figsize=(grid.shape[1] / 100, grid.shape[0] / 100), dpi=100)
-    plt.imshow(grid, cmap='binary')
-    plt.axis('off')
-    plt.gca().set_xticks([])
-    plt.gca().set_yticks([])
-    plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
-    plt.savefig(filename, bbox_inches='tight', pad_inches=0)
-    plt.close()
+    # plt.figure(figsize=(grid.shape[1] / 100, grid.shape[0] / 100), dpi=100)
+    # plt.imshow(grid, cmap='binary')
+    # plt.axis('off')
+    # plt.gca().set_xticks([])
+    # plt.gca().set_yticks([])
+    # plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+    # plt.savefig(filename, bbox_inches='tight', pad_inches=0)
+    # plt.close()
+    cv2.imwrite(filename, np.uint8(grid)*255)
 
 
 def create_video(frames, output_filename):
     frame = cv2.imread(frames[0])
     height, width, layers = frame.shape
 
-    video = cv2.VideoWriter(output_filename, cv2.VideoWriter_fourcc(*'avc1'), 10, (width, height)) #mp4v
+    video = cv2.VideoWriter(output_filename, cv2.VideoWriter_fourcc(*'avc1'), 60, (width, height)) #mp4v
 
     for frame in frames:
         video.write(cv2.imread(frame))
